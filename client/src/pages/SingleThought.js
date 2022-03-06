818d2c27734ca1687bd0dcbae3bf83e9ee48c04c
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-03-05 19:05:25 -0500

    reactions populating when thought clicked on

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
-//import ReactionList from '../components/ReactionList';
+import ReactionList from '../components/ReactionList';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
@@ -33,6 +32,8 @@ const SingleThought = props => {
           <p>{thought.thoughtText}</p>
         </div>
       </div>
+
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
     </div>
   );
 };
