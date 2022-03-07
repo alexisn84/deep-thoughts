e7794578e273ca59369461f5bd095425db699192
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-03-07 16:04:39 -0500

    header changes pending logged in or not

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const Login = (props) => {
@@ -23,7 +24,7 @@ const Login = (props) => {
 
     try {
       const { data } = await login({
-        variables: { ...formState }
+        variables: { ...formState },
       });
 
       Auth.login(data.login.token);
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -4,7 +4,7 @@ class AuthService {
     //retrieve data saved in token
     getProfile() {
         return decode(this.getToken());
-    }
+      }
 
     //check if user is still logged in
     loggedIn() {
@@ -12,7 +12,7 @@ class AuthService {
         const token = this.getToken();
         
         //use type coesion to scheck if token is not undefied and the token is not expired
-        return !!token && this.isTokenExpired(token);
+        return !!token && !this.isTokenExpired(token);
     }
 
     //check if the token has expired
@@ -20,20 +20,18 @@ class AuthService {
         try {
             const decoded = decode(token);
             if (decoded.exp < Date.now() / 1000) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (err) {
+              return true;
+            } else return false;
+          } catch (err) {
             return false;
+          }
         }
-    }
 
     //retrieve token from localStorage
     getToken() {
         //retrieves the user token from localStorage
         return localStorage.getItem('id_token');
-    }
+  }
 
     //set token to localStorage and reload page to homepage
     login(idToken) {
