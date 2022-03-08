4569b0248e8bbb9475b4a0344b3ca57aaedc619d
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-03-08 14:04:27 -0500

    thoughts auto populating both home and profile pages

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -5,26 +5,63 @@ import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 const ThoughtForm = () => {
     const [thoughtText, setText] = useState('');
-  const [characterCount, setCharacterCount] = useState(0);
+    const [characterCount, setCharacterCount] = useState(0);
 
-   // update state based on form input changes
-  const handleChange = (event) => {
-    if (event.target.value.length <= 280) {
-      setText(event.target.value);
-      setCharacterCount(event.target.value.length);
+    const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+      update(cache, { data: { addThought } }) {
+        try {
+        //could potentially not exist yet, so wrap in a try...catch
+        const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+        //prepend the newest thougth to the front of the array
+        cache.writeQuery({
+          query: QUERY_THOUGHTS,
+          data: { thoughts: [addThought, ...thoughts] }
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      //update me objects cache, appending new thought to end of array
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
+      });
     }
-  };
+  });
 
-  const handleFormSubmit = async event => {
-      event.preventDefault();
-      setText('');
-      setCharacterCount(0);
-  };
+    // update state based on form input changes
+    const handleChange = (event) => {
+      if (event.target.value.length <= 280) {
+        setText(event.target.value);
+        setCharacterCount(event.target.value.length);
+      }
+    };
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+          //add thought to db
+          await addThought({
+            variables: { thoughtText }
+          });
+
+          //clear form value
+          setText('');
+          setCharacterCount(0);
+        } catch (e) {
+          console.error(e);
+        }
+        
+    };
 
     return (
         <div>
-            <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+            <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
+                {error && <span className="ml-2">Something went wrong...</span>}
                 </p>
                 <form className="flex-row justify-center justify-space-between-md align-stretch"
                     onSubmit={handleFormSubmit}>
